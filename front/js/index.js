8b14ac05a06fb0b707263c772d8e5801832e830c
Fibre44 <denis.nicolas@hotmail.fr>
2022-05-25 22:23:58 +0200

    feat : mise à jour pour mise en ligne sur apache

--- front/js/index.js
+++ front/js/index.js
@@ -1,58 +1,66 @@
-function getAllKanaps () {
-    fetch ("http://localhost:3000/api/products")
-    .then(function(res) {
-        if (res.ok) {
-            return res.json();
-        }        
-    })
-    .catch(function (err){
-        console.error("erreur de connexion au serveur/conversion de la réponse ",err);
-    })
-    .then(function(kanaps) {
-      
-         addKanaps(kanaps);      
-    })
+function getAllKanaps() {
+    fetch("http://localhost:3000/api/products")
+        .then(function (res) {
+            if (res.ok) {
+                return res.json();
+            }
+        })
+        .catch(function (err) {
+            console.error("erreur de connexion au serveur/conversion de la réponse ", err);
+        })
+        .then(function (kanaps) {
 
-    .catch(function (err){
+            addKanaps(kanaps);
+        })
 
-        console.error("Erreur lors de l ecriture sur la page ", err);           
+        .catch(function (err) {
 
-    })
+            console.error("Erreur lors de l ecriture sur la page ", err);
 
-    .catch(function(err) {
-        console.error("Impossible de joindre le serveur",err);
-  });
+        })
+
+        .catch(function (err) {
+            console.error("Impossible de joindre le serveur", err);
+        });
 }
 
 getAllKanaps()
 
+async function test() {
+    const data = await fetch("http://localhost:3000/api/products")
+    const json = await data.json();
+    console.log(json);
+}
 
-function addKanaps (kanaps){
-    for (kanap of kanaps){
-        
-    let urlKanap = document.createElement('a');
-    document.getElementById('items').appendChild(urlKanap);
-    urlKanap.href = './../html/product.html?id='+kanap._id;
+test();
 
-    let articleKanap = document.createElement('article');
-    urlKanap.appendChild(articleKanap);
 
-    let articleImg = document.createElement('img');
-    articleKanap.appendChild(articleImg);
-    articleImg.src = kanap.imageUrl;
-    articleImg.alt = kanap.altTxt;
 
-    let nameKanap = document.createElement('h3');
-    articleKanap.appendChild(nameKanap);
-    nameKanap.classList.add('productname');
-    nameKanap.textContent = kanap.name;
+function addKanaps(kanaps) {
+    for (kanap of kanaps) {
 
-    let descriptionKanap = document.createElement('p');
-    articleKanap.appendChild(descriptionKanap);
-    descriptionKanap.classList.add('productDescription');
-    descriptionKanap.textContent = kanap.description;
-  
+        let urlKanap = document.createElement('a');
+        document.getElementById('items').appendChild(urlKanap);
+        urlKanap.href = './/html/product.html?id=' + kanap._id;
+
+        let articleKanap = document.createElement('article');
+        urlKanap.appendChild(articleKanap);
+
+        let articleImg = document.createElement('img');
+        articleKanap.appendChild(articleImg);
+        articleImg.src = kanap.imageUrl;
+        articleImg.alt = kanap.altTxt;
+
+        let nameKanap = document.createElement('h3');
+        articleKanap.appendChild(nameKanap);
+        nameKanap.classList.add('productname');
+        nameKanap.textContent = kanap.name;
+
+        let descriptionKanap = document.createElement('p');
+        articleKanap.appendChild(descriptionKanap);
+        descriptionKanap.classList.add('productDescription');
+        descriptionKanap.textContent = kanap.description;
+
     }
-  }
+}
 
-  
